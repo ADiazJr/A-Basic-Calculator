3a3a0a1be52fb8f84e47e9c5bb2c61eb31ad1a73
unknown <arturodiazmay@gmail.com>
2023-01-19 05:06:25 -0600

    Created Calculator.java with Calculator Class + Created add and subtract methods

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+    public Calculator() {
+  
+    }
+  
+    public int add(int a, int b) {
+      int sum = a + b;
+      return sum;
+    }
+  
+    public int subtract(int a, int b) {
+      int subtraction = a - b;
+      return subtraction;
+    }
+  
+    public void main(String[] args) {
+        
+    }
+  }
