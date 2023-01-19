b9a6a2a4be73f0339cdcbba4201f3cb920ea6493
unknown <arturodiazmay@gmail.com>
2023-01-19 05:20:25 -0600

    Created myCalculator Object to use method + Added Memory Feature

--- Calculator.java
+++ Calculator.java
@@ -1,35 +1,45 @@
 public class Calculator {
-
-    public Calculator() {
-  
-    }
-  
-    public int add(int a, int b) {
-      int sum = a + b;
-      return sum;
-    }
-  
-    public int subtract(int a, int b) {
-      int subtraction = a - b;
-      return subtraction;
-    }
-  
-    public int mulitply(int a, int b) {
-      int multiplication = a * b;
-      return multiplication;
-    }
-  
-    public int divide(int a, int b) {
-      int division = a/b;
-      return division;
-    }
-  
-    public int modulo(int a, int b) {
-      int modulo = a % b;
-      return modulo;
-    }
-  
-    public static void main(String[] args) {
-      Calculator myCalculator = new Calculator();
-    }
-  }
+     int memory;
+     
+     public Calculator() {
+       
+     }
+   
+     public int add(int a, int b) {
+       int sum = a + b;
+       return sum;
+     }
+   
+     public int subtract(int a, int b) {
+       int subtraction = a - b;
+       return subtraction;
+     }
+   
+     public int mulitply(int a, int b) {
+       int multiplication = a * b;
+       return multiplication;
+     }
+   
+     public int divide(int a, int b) {
+       int division = a/b;
+       return division;
+     }
+   
+     public int modulo(int a, int b) {
+       int modulo = a % b;
+       return modulo;
+     }
+   
+     public void addToMemory(int numberToAdd) {
+       System.out.println("The memory now holds " + numberToAdd);
+       memory = numberToAdd;
+     }
+   
+     public static void main(String[] args) {
+       Calculator myCalculator = new Calculator();
+       System.out.println(myCalculator.add(5,7));
+       System.out.println(myCalculator.subtract(45,11));
+       myCalculator.addToMemory(34);
+       System.out.println(myCalculator.memory);
+     }
+   }
